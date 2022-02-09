4ec5eb7581d6137d8cccc4f551775beeebcca9e5
Aiden Miller <bonsoirelliot@gmail.com>
2022-02-09 11:24:16 -0500

    Add project files.

---
+++ WebApplication1/Extensions/ServiceExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApplication1.Extensions
+{
+    public static class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) =>
+             services.AddCors(options =>        // very basic CORS implementation
+             {
+                 options.AddPolicy("CorsPolicy", builder =>
+                 builder.AllowAnyOrigin()
+                 .AllowAnyMethod()
+                 .AllowAnyHeader());
+             });
+        public static void ConfigureIISIntegration(this IServiceCollection services) =>
+            services.Configure<IISOptions>(options =>
+            {
+            });
+    }
+}
